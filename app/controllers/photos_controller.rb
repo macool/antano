2b1c2424f16c953d768a8ac3ead7e1ca5e079ffc
Mario Andrés Correa <a@macool.me>
2014-04-29 16:52:47 -0500

    show public images sorted

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
-  expose_decorated(:photos) { Photo.published }
+  expose_decorated(:photos) { Photo.sorted.published }
   expose_decorated(:photo)
-  
+
   def show
     self.photo = photos.find params[:id]
   end
