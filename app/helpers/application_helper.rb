a0d53800e8892150d0efb7b8bc119511ca699cca
Mario Andrés Correa <a@macool.me>
2014-04-29 20:55:14 -0500

    `twitter_follow_button` is a required method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
       end.join.html_safe
     end
   end
+
+  def twitter_follow_button
+    html = <<-BUTTON
+    <a href="https://twitter.com/cuxibamba" class="twitter-follow-button" data-show-count="false" data-lang="es" data-dnt="true">Seguir a @cuxibamba</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+BUTTON
+    html.html_safe
+    nil
+  end
 end
