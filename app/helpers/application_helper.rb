c93dd5c29c7ad5318b115982c07ea7b958499134
Mario Andrés Correa <a@macool.me>
2014-05-03 18:06:18 -0500

    unused markup / templates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,13 +15,4 @@ module ApplicationHelper
     disqus_identifier = "#{object.class.name.underscore}/#{object.id}"
     content_tag(:div, nil, id: "disqus_thread") + javascript_tag("disqusHelper.load('#{disqus_identifier}');")
   end
-
-  def twitter_follow_button
-    html = <<-BUTTON
-    <a href="https://twitter.com/cuxibamba" class="twitter-follow-button" data-show-count="false" data-lang="es" data-dnt="true">Seguir a @cuxibamba</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-BUTTON
-    html.html_safe
-    nil
-  end
 end
