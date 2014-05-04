77ffa649ffa61e659eae3ed5def7be1a2d6e7c30
Mario Andrés Correa <a@macool.me>
2014-05-04 00:20:27 -0500

    use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140503212853) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "photos", force: true do |t|
     t.string   "title",                   null: false
     t.string   "image"
