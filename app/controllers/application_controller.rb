be427cedad920f7c94637873a8a206a7c47ef43a
Mario Andrés Correa <a@macool.me>
2014-05-09 11:05:29 -0500

    add FB OG meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  expose(:og_url) { ENV["HOST_URL"] }
+  expose(:og_title) { "#LoxaDeAntaño" }
+  expose(:og_image) { "https://pbs.twimg.com/profile_images/2317193138/image.jpg" }
+
   # decent exposure strong parameters
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,10 @@ class PhotosController < ApplicationController
   expose_decorated(:photos) { Photo.sorted.published }
   expose_decorated(:photo)
 
+  expose(:og_url) { photo.send(:permalink) }
+  expose(:og_title) { photo.title }
+  expose(:og_image) { photo.image_url(:public) }
+
   def show
     self.photo = photos.find params[:id]
   end
